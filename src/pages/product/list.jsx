da6857beaa3e2c827ce8898b9040ff296a8b2baf
Mahdi Pourtalebi <mahdi.pourtalebi@gmail.com>
2024-06-21 19:26:09 +0330

    added loading

--- src/pages/product/list.jsx
+++ src/pages/product/list.jsx
@@ -1,28 +1,55 @@
 import Header from "@/components/Header/header"
 import List from "@/components/product/list"
 import Sidebar from "@/components/sidebar/sidebar"
-import { useSelector } from "react-redux"
+import { useEffect } from "react"
+import { useSelector, useDispatch } from "react-redux"
 import { useMediaQuery } from "react-responsive"
+import { setProducts } from "@/store/slices/productList"
+import ScaleLoader from "react-spinners/ScaleLoader"
+import { within } from "./../../../node_modules/@popperjs/core/dist/esm/utils/within"
 
 export default function ListPage() {
-	const products = useSelector((state) => state.productList.products)
-	const isDesktop = useMediaQuery({ query: "(min-width: 1150px)" })
-	const isLocading = useSelector((state) => state.productList.isLoading)
+	const dispatch = useDispatch()
 
+	const products = useSelector((state) => state.productList.shownProducts)
+	const isDesktop = useMediaQuery({ query: "(min-width: 1150px)" })
+	const isLoading = useSelector((state) => state.productList.isLoading)
+	console.log(isLoading)
+
+	useEffect(() => {
+		fetch("https://fakestoreapi.com/products")
+			.then((res) => res.json())
+			.then((data) => {
+				console.log(data)
+				setTimeout(() => {
+					dispatch(setProducts(data))
+				}, 1000)
+			})
+	}, [])
 	return (
 		<>
-			<Header shouldShowMenu={!isDesktop} menu={<Sidebar.productList />} />
+			<Header shouldShowMenu={!isDesktop} menu={<Sidebar.productList />}>
+				<span className="text-2xl font-bold">Bold Shop</span>
+			</Header>
 			<div className="flex flex-row">
-				{isDesktop && (
-					<div className="flex flex-wrap items-center h-96 justify-center p-4 m-4 bg-gray-100">
-						<Sidebar>
-							<Sidebar.productList />
-						</Sidebar>
+				<Sidebar>
+					<Sidebar.productList />
+				</Sidebar>
+				{isLoading && (
+					<div className="w-full h-[85vh] flex flex-wrap items-center justify-center p-4 m-4 bg-gray-100">
+						<ScaleLoader
+							color="#36d7b7"
+							width="15px"
+							height="150px"
+							margin="10px"
+						/>
 					</div>
 				)}
-				<div className="flex flex-wrap items-center justify-center p-4 m-4 bg-gray-100">
-					<List products={products} />
-				</div>
+				{!isLoading && (
+					<div className="flex flex-wrap items-center justify-center p-4 m-4 bg-gray-100">
+						<List products={products} />
+					</div>
+				)}
 			</div>
 		</>
 	)
--- src/store/slices/productList.js
+++ src/store/slices/productList.js
@@ -1,19 +1,54 @@
 import { createSlice } from "@reduxjs/toolkit"
-import defaultProducts from "@/mocks/products"
 
 const productList = createSlice({
 	name: "productList",
 	initialState: {
-		products: defaultProducts,
-		loading: true,
+		products: [],
+		shownProducts: [],
+		isLoading: true,
 		filter: {},
 	},
 	reducers: {
 		setProducts(state, action) {
 			state.products = action.payload
+			state.shownProducts = action.payload
+			state.isLoading = false
 		},
 		setFilter(state, action) {
 			state.filter = action.payload
+			state.shownProducts = state.products.filter((product) => {
+				const { minPrice, maxPrice, minRating, maxRating } = state.filter
+				if (minPrice && product.price < minPrice) return false
+				if (maxPrice && product.price > maxPrice) return false
+				if (minRating && product.rating.rate < minRating) return false
+				if (maxRating && product.rating.rate > maxRating) return false
+				return true
+			})
+			if (state.filter.sort === "price") {
+				state.shownProducts = state.shownProducts.sort(
+					(a, b) => a.price - b.price
+				)
+			} else if (state.filter.sort === "-price") {
+				state.shownProducts = state.shownProducts.sort(
+					(a, b) => b.price - a.price
+				)
+			} else if (state.filter.sort === "rating") {
+				state.shownProducts = state.shownProducts.sort(
+					(a, b) => a.rating.rate - b.rating.rate
+				)
+			} else if (state.filter.sort === "-rating") {
+				state.shownProducts = state.shownProducts.sort(
+					(a, b) => b.rating.rate - a.rating.rate
+				)
+			} else if (state.filter.sort === "name") {
+				state.shownProducts = state.shownProducts.sort((a, b) =>
+					a.title.localeCompare(b.title)
+				)
+			} else if (state.filter.sort === "-name") {
+				state.shownProducts = state.shownProducts.sort((a, b) =>
+					b.title.localeCompare(a.title)
+				)
+			}
 		},
 	},
 })
